8c5ebfb23213c4bcca75c5c5df1254ff7276580d
Puntherline <53500972+Puntherline@users.noreply.github.com>
2020-02-28 05:35:27 +0100

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,8 @@
+Locales['en'] = {
+    ['blipname'] = 'car Wash',
+    ['press_button'] = 'press ~INPUT_VEH_HORN~ to clean your vehicle for ~g~$',
+    ['cleaning_vehicle'] = 'your vehicle is being cleaned.',
+    ['cleaned_vehicle'] = 'your vehicle has been cleaned for ~g~$',
+    ['not_enough_money'] = 'you don\'t have enough money, you need ~r~$',
+    ['no_wash_needed'] = 'your vehicle doesn\'t need a wash.'
+}
